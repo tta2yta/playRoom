92b31edf1e4b31dcd6c28df67a5a89823d79328f
tta2yta@gmail.com <tta2yta@gmail.com>
2020-07-30 13:41:14 +0300

    modify matchestab

--- src/components/MatchesTab.js
+++ src/components/MatchesTab.js
@@ -4,36 +4,37 @@ import MatchGameTab from './MatchGameTab'
 export default function MatchesTab() {
     return (
         <>
-        <div className="row no-gutters">
-        <div className="col-md-6 matchleft ">
-        <div className="row ">
-        <div className=" col-md-6">
+        <div className="row justify-content-center" >
        
+            <table className="tblgameres" >
+            <thead></thead><tbody>
+                <tr>
+                    <td className="td-vic" align="center">VICTORY</td>
+                    <td className="td-vic-bl" rowSpan="2"><span></span></td>
+                    <td></td>
+                    </tr>
+                <tr>
+                    <td align="center" >TEAM ALHPA</td>
+                    <td className="td-vic-res" >2</td>
+                    <td > : </td>
+                </tr>
+                </tbody>
+            </table>
+            <table className="tblgameres" width="30%">
+            <thead></thead><tbody>
+                <tr>
+                    <td></td>
+                    <td className="td-vic-bl" rowSpan="2"><span></span></td>
+                    <td className="td-vic" align="center">DEFEAT</td>
+                    </tr>
+                <tr>
+                    <td > &nbsp;1</td>
+                    <td className="td-vic-res" align="center">TEAM BETA</td>
+                </tr>
+                </tbody>
+            </table>
         </div>
-        
-        <div className=" col-md-6 tes">
-            <div className="row matchleft ml-auto">
-        <ul className=""><li>VICTORY</li>
-        <li>ALPHA</li></ul>
-        <div><span></span><strong className="sc-l">2 &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;:</strong></div>
-        </div>   
-        </div>
-        </div>
-</div>
        
-        <div className="col-md-6 ">
-      
-        <div className=" col-md-6 tes">
-            <div className="row matchright">
-            <div><strong className="sc-r">1</strong><span></span></div>
-        <ul className=""><li>VICTORY</li>
-        <li>ALPHA</li></ul></div>
-        <div className="row col-md-6 tes">
-        
-        </div>
-        </div>
-        </div>
-        </div>
         
         {/* <lable>VICTORY Team Alpha</lable></div>
     <div className="col-md-3"><span></span><strong>2</strong></div>
